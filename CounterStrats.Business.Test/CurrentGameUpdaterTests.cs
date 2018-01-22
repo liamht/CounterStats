428c7f895a54ba1cf32c195ec6b81fddbcf8e0c5
liamht <ljht93@hotmail.com>
2018-01-22 19:06:00 +0000

    added test project. Code is not yet testable

---
+++ CounterStrats.Business.Test/CurrentGameUpdaterTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit;
+using CounterStats.Business;
+using NUnit.Framework;
+
+namespace CounterStrats.Business.Test
+{
+    public class CurrentGameUpdaterTests
+    {
+        private CurrentGameUpdater _subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _subject = new CurrentGameUpdater();
+        }
+    }
+}
