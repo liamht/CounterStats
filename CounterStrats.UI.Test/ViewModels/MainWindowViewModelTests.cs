5e0eeb648c49ebfad71954992f9a57b813c7cd67
liamht <ljht93@hotmail.com>
2018-01-25 22:51:44 +0000

    11 - Added first unit tests for main menu item selection

---
+++ CounterStrats.UI.Test/ViewModels/MainWindowViewModelTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CounterStats.UI.ViewModels;
+using CounterStats.UI.Views.Elements;
+using Moq;
+using NUnit.Framework;
+
+namespace CounterStrats.UI.Test.ViewModels
+{
+    public class MainWindowViewModelTests
+    {
+        private MainWindowViewModel _subject;
+        private bool _hasMenuItemMethodBeenCalled;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var mainMenu = new MainMenu();
+
+            _subject = new MainWindowViewModel(mainMenu);
+        }
+
+        [Test]
+        public void SelectedItem_WhenChanged_CallsOnClickAction()
+        {
+            var menuItem = new MenuItem() {Text = "test", OnClick = () => MainMenuItemMethod()};
+            _subject.SelectedMenuItem = menuItem;
+
+            Assert.IsTrue(_hasMenuItemMethodBeenCalled);
+        }
+           
+
+        private void MainMenuItemMethod()
+        {
+            _hasMenuItemMethodBeenCalled = true;
+        }
+    }
+}
