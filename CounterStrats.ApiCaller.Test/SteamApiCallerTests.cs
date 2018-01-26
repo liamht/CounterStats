21bac7e6ec6d568e91d9ba45ead86f60b11a9808
liamht <ljht93@hotmail.com>
2018-01-26 22:12:47 +0000

    11 [ADD] added SteamApiCallerTests class

---
+++ CounterStrats.ApiCaller.Test/SteamApiCallerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CounterStats.ApiCaller;
+using CounterStats.ApiCaller.HttpWebClient;
+using Moq;
+using NUnit.Framework;
+
+namespace CounterStrats.ApiCaller.Test
+{
+    public class SteamApiCallerTests
+    {
+        private SteamApiCaller _subject;
+        private Mock<HttpWebClient> _client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _client = new Mock<HttpWebClient>();
+            
+            _subject = new SteamApiCaller(_client.Object);
+        }
+    }
+}
