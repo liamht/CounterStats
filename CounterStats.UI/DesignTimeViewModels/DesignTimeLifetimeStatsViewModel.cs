7e2e8eab2205b64c52f621874c7f8047dfbeb9fa
liamht <ljht93@hotmail.com>
2018-01-27 18:32:58 +0000

    Added Viewmodels

---
+++ CounterStats.UI/DesignTimeViewModels/DesignTimeLifetimeStatsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CounterStats.UI.ViewModels;
+
+namespace CounterStats.UI.DesignTimeViewModels
+{
+    public class DesignTimeLifetimeStatsViewModel : LifetimeStatsViewModel
+    {
+        public DesignTimeLifetimeStatsViewModel()
+        {
+        }
+    }
+}
