18ba90a457150b4921c57d69e945223131bfc94c
liamht <ljht93@hotmail.com>
2018-01-27 18:28:34 +0000

    Added lifetime stats page

---
+++ CounterStats.UI/Views/LifetimeStats/LifetimeStatsPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Controls;
+using CounterStats.UI.ViewModels;
+
+namespace CounterStats.UI.Views.LifetimeStats
+{
+    /// <summary>
+    /// Interaction logic for CurrentGamePage.xaml
+    /// </summary>
+    public partial class LifetimeStatsPage : UserControl
+    {
+        public LifetimeStatsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
