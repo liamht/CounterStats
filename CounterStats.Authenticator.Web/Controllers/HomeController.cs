a327066343231a2b4c90827d8e4cda1199a0907e
liamht <ljht93@hotmail.com>
2018-01-31 19:07:52 +0000

    Added the most basic website you've ever seen. So that I can get api keys through OAuth

---
+++ CounterStats.Authenticator.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CounterStats.Authenticator.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string steamId)
+        {
+            return Content(steamId);
+        }
+    }
+}
